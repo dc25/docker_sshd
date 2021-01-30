c1a5bf33728b2e48bfabd9937d430e53604a60ee
Dave Compton <davecompton7@gmail.com>
2021-01-30 12:56:30 -0800

    minor cleanup of setup_ssh.sh script; added comments changed order of operations

--- setup_ssh.sh
+++ setup_ssh.sh
@@ -1,10 +1,17 @@
 #! /bin/bash
 
-set -x
-
-export PUBLIC_KEY="$1"
-
+#install and configure the sshd server
 export DEBIAN_FRONTEND=noninteractive
+sudo apt-get update && sudo apt-get install -y \
+    openssh-server 
+
+echo "X11UseLocalhost no" | sudo tee -a /etc/ssh/sshd_config
+echo "Port 20022" | sudo tee -a /etc/ssh/sshd_config
+
+#set up public key login
+mkdir -p ~/.ssh
+echo "$1" > ~/.ssh/authorized_keys
+chmod 600 ~/.ssh/authorized_keys
 
 ## create a script to run a program and stay in foreground.
 cat << DONE | sudo tee -a /runexec.sh
@@ -13,14 +20,4 @@ cat << DONE | sudo tee -a /runexec.sh
 sleep 10000000
 DONE
 sudo chmod 777 /runexec.sh
-
-sudo apt-get update && sudo apt-get install -y \
-    openssh-server 
-
-echo "X11UseLocalhost no" | sudo tee -a /etc/ssh/sshd_config
-echo "Port 20022" | sudo tee -a /etc/ssh/sshd_config
-
-mkdir -p ~/.ssh
-echo "$PUBLIC_KEY" > ~/.ssh/authorized_keys
-chmod 600 ~/.ssh/authorized_keys
 
